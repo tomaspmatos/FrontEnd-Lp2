2748540fcbaba760244c591f91cb08c6f17e2893
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2023-01-12 05:47:04 -0300

    listagem funcinarios pronta

--- src/views/listagem-funcionarios.js
+++ src/views/listagem-funcionarios.js
@@ -16,7 +16,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import axios from 'axios';
 import { BASE_URL } from '../config/axios';
 
-const baseURL = `${BASE_URL}/usuarios`;
+const baseURL = `${BASE_URL}/funcionarios`;
 
 function ListagemFuncionarios() {
   const navigate = useNavigate();
